0e9af2512020832a901d350acac73185e4c101e6
Ankit Luthra <95644464+aluthra999@users.noreply.github.com>
2023-02-26 16:47:32 -0400

    Add files via upload

---
+++ phonetic_name.py
@@ -0,0 +1,35 @@
+phonetic_dict = {
+    "A": "Alpha",
+    "B": "Bravo",
+    "C": "Charlie",
+    "D": "Delta",
+    "E": "Echo",
+    "F": "Foxtrot",
+    "G": "Golf",
+    "H": "Hotel",
+    "I": "India",
+    "J": "Juliet",
+    "K": "Kilo",
+    "L": "Lima",
+    "M": "Mike",
+    "N": "November",
+    "O": "Oscar",
+    "P": "Papa",
+    "Q": "Quebec",
+    "R": "Romeo",
+    "S": "Sierra",
+    "T": "Tango",
+    "U": "Uniform",
+    "V": "Victor",
+    "W": "Whiskey",
+    "X": "Xray",
+    "Y": "Yankee",
+    "Z": "Zulu"
+}
+
+
+your_name = input("Type your name: ")
+
+phonetic_code = [phonetic_dict[letter.upper()] for letter in your_name]
+
+print(phonetic_code)
